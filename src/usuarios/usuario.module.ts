bc766aa4fbea3ab081f0adbb5e9f4886a55974ef
Joe <joaokingteam@hotmail.com>
2023-08-25 16:08:30 -0300

    services

--- src/usuarios/usuario.module.ts
+++ src/usuarios/usuario.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { UsuarioController } from './controllers/usuario.controller';
 import { AuthenticateUsuarioController } from './controllers/auth.controller';
 import { ListaUsuarioController } from './controllers/lista-usuarios.controller';
+import { UsersService } from './services/users.service';
 
 
 @Module({
     controllers: [UsuarioController, AuthenticateUsuarioController, ListaUsuarioController],
+    providers: [UsersService],
 })
 
 export class UsuarioModule { }
