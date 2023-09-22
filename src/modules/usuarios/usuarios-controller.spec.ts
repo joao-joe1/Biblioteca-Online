5c05d526b94e295476d36536ea83a8be22b47b75
Joe <joaokingteam@hotmail.com>
2023-09-22 16:46:57 -0300

    s

--- src/modules/usuarios/usuarios-controller.spec.ts
+++ src/modules/usuarios/usuarios-controller.spec.ts
@@ -1,47 +1,51 @@
-import { PrismaService } from "src/prisma/prisma.service";
 import { UsuarioController } from "./controllers/auth.controller"
+import { PrismaService } from "src/prisma/prisma.service";
 import { AuthService } from "./services/auth.service"
 
 describe('AuthController', () => {
     let usuarioController: UsuarioController;
-    let authService: AuthService;
     let prismaService: PrismaService;
+    let authService: AuthService;
 
     beforeEach(() => {
-        authService = new AuthService();
         prismaService = new PrismaService();
+        authService = new AuthService();
         usuarioController = new UsuarioController(prismaService, authService)
     })
+
+    afterAll(async () => {
+        await prismaService.$disconnect();
+    })
+
+    it('deve autenticar um usuário com credenciais válidas', async () => {
+
+        const userData = {
+            name: 'Test User',
+            email: 'test@example.com',
+            password: 'password123',
+            cep: '35179-000',
+            cidade: 'ipatinga',
+            bairro: 'bom pastor',
+            rua: 'morango',
+            telefone: '31986876109',
+        }
+
+        const createUser = await prismaService.users.create({
+            data: userData
+        })
+
+        expect(createUser).toBeTruthy()
+
+        const loginUser = {
+            email: userData.email,
+            password: userData.password
+        }
+
+        const result = await usuarioController.login(loginUser)
+
+        expect(result).toBeTruthy()
+        expect(result.message).toBe('Autenticado com sucesso!')
+    });
 })
 
-it('deve cadastrar um novo usuário com dados válidos', async () => {
 
-});
-
-it('deve retornar erro ao tentar cadastrar um usuário com email já em uso', async () => {
-
-});
-
-it('deve retornar erro ao tentar cadastrar um usuário com dados inválidos', async () => {
-
-});
-
-it('deve autenticar um usuário com credenciais válidas', async () => {
-
-});
-
-it('deve retornar erro ao tentar autenticar um usuário com email que não existe', async () => {
-
-});
-
-it('deve retornar erro ao tentar autenticar um usuário com senha incorreta', async () => {
-
-});
-
-it('deve realizar o logout de um usuário autenticado', async () => {
-
-});
-
-it('deve retornar erro ao tentar realizar o logout sem token válido', async () => {
-
-});
